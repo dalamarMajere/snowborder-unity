66a545404dd678b68c2f5feceb2e19d4732af75c
dalamarMajere <vitastudio01@gmail.com>
2022-10-20 17:24:21 +0200

    Created surfing and flipping

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float torqueAmount;
+    
+    
+    private Rigidbody2D rigidBody2D;
+
+    private void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody2D.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody2D.AddTorque(-torqueAmount);
+        }
+    }
+}
