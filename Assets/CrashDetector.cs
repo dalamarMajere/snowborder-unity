f623e3b6306e176f6ea05f27b680193534aaffa2
Violetta Bayeva <vitastudio01@gmail.com>
2022-10-20 08:54:46 -0700

    Game end (#5)
    * Created winning condition
    
    * Created game crash and win detector

--- Assets/CrashDetector.cs
+++ Assets/CrashDetector.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FinishLine : MonoBehaviour
+public class CrashDetector : MonoBehaviour
 {
     [SerializeField] private float reloadDelay;
     
-    private const string PlayerTag = "Player";
-    
+    private const string GroundTag = "Ground";
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag(PlayerTag))
+        if (col.CompareTag(GroundTag))
         {
             Invoke(nameof(ReloadScene), reloadDelay);
         }
     }
-
+    
     private void ReloadScene()
     {
         SceneManager.LoadScene(0);
