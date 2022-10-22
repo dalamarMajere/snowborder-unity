6a2e5f900467311b4aff45c935432951aa07c9d2
dalamarMajere <vitastudio01@gmail.com>
2022-10-22 19:09:36 +0200

    Moved scripts to scripts folder

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float torqueAmount;
+    [SerializeField] private float baseSpeed;
+    [SerializeField] private float boostSpeed;
     
     private Rigidbody2D rigidBody2D;
+    private SurfaceEffector2D surfaceEffector2D;
 
     private void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     private void Update()
+    {
+        RotatePlayer();
+        Boost();
+    }
+
+    private void Boost()
+    {
+        surfaceEffector2D.speed = Input.GetKey(KeyCode.UpArrow) ? boostSpeed : baseSpeed;
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
